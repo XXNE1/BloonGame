2f6cd647f0f67625405bb4238c8e3372041263c0
jonas.elsner <jonas.elsner@122-020.list.intern>
2022-05-13 12:11:03 +0200

    lol

--- BloonGame/src/bloongame/Fenster.java
+++ BloonGame/src/bloongame/Fenster.java
@@ -1,5 +1,44 @@
 package bloongame;
 
-public class Fenster {
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+
+public class Fenster extends JFrame{
     
+    private JPanel statistikpanel = new JPanel();
+    private JPanel gamepanel = new JPanel();
+    private JLabel lblEingabe = new JLabel();
+    private JButton buttonst = new JButton();
+    private Timer t;
+    
+    
+    
+     public Fenster() {
+         
+        this.setLayout(null);
+        this.setSize(600,400);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Bloongame");
+        this.getContentPane().setBackground(Color.WHITE);
+        this.add(statistikpanel);
+        this.setVisible(true);
+        
+    //Startbutton
+        buttonst.setLocation(10, 330);
+        buttonst.setSize(150, 60);
+        buttonst.setText("Start");
+        this.add(buttonst);
+        
+        buttonst.addActionListener((ActionEvent ae) -> {  
+            System.out.print("Start" + "\n");
+            this.StarteSpiel();
+            });
+
+}
 }
