2a07721dd43fce7476fed4b4b74abf1508161783
justus.ostermann <justus.ostermann@122-018.list.intern>
2022-05-13 12:14:22 +0200

    Commit

--- BloonGame/src/bloongame/BloonGame.java
+++ BloonGame/src/bloongame/BloonGame.java
@@ -1,11 +1,5 @@
 package bloongame;
 
-//Wo ist die Galsfaser?
-//Wo sind sie?
-
-//Ja wo laufen sie hin?
-//Unsere Vorräte schwinden dahin
-
 public class BloonGame {
 
     public static void main(String[] args) {
