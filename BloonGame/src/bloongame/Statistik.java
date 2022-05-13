374fe20cfed0fa6b316b6ddb7a2e926c1c2163be
justus.ostermann <justus.ostermann@122-018.list.intern>
2022-05-13 12:37:56 +0200

    Commit

--- BloonGame/src/bloongame/Statistik.java
+++ BloonGame/src/bloongame/Statistik.java
@@ -1,6 +1,20 @@
 package bloongame;
 
-public class Statistik {
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Statistik extends JPanel {
+
+    private JLable time = new JLabel();
+    private JLable leben = new JLabel();
+    private JLable counter = new JLabel();
+
+public Statistik() {
 
 
+    
+
+
+
+    }
 }
