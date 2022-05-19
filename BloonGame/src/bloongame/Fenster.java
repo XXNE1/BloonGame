ef3afceb954ea368cae4f2c88082f762fb37daf5
XXNE1 <55995962+XXNE1@users.noreply.github.com>
2022-05-19 17:30:21 +0200

    Jonas changes

--- BloonGame/src/bloongame/Fenster.java
+++ BloonGame/src/bloongame/Fenster.java
@@ -8,16 +8,13 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-
-public class Fenster extends JFrame{   
-   
+public class Fenster extends JFrame{  
     
     private JButton buttonst = new JButton();
+    private JButton buttonrest = new JButton();
     private Timer t;
     
-    private Game game = new Game();
-    
-    
+    private Game game = new Game();      
     
     public Fenster() {
          
@@ -27,11 +24,14 @@ public class Fenster extends JFrame{
         this.setTitle("Bloongame");
         this.getContentPane().setBackground(Color.WHITE);        
         this.setVisible(true);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH); 
         
         //Startbutton
         buttonst.setLocation(10, 420);
         buttonst.setSize(150, 60);
         buttonst.setText("Start");
+        buttonst.setBackground(Color.DARK_GRAY);
+        buttonst.setForeground(Color.yellow);
         this.add(buttonst);
         
         buttonst.addActionListener((ActionEvent ae) -> {  
@@ -42,14 +42,19 @@ public class Fenster extends JFrame{
         game.setLocation(10,10);
         this.add(game);
         
+        //Restartbutton
+        buttonrest.setLocation(10,490);
+        buttonrest.setSize(150,60);
+        buttonrest.setText("Restart");
+        this.add(buttonrest);
+        buttonrest.setBackground(Color.DARK_GRAY);
+        buttonrest.setForeground(Color.yellow);
+        buttonrest.setEnabled(false);        
     }
     
     public void starteSpiel(){
         buttonst.setEnabled(false);        
-        game.startGame();
-       
-        
+        game.startGame(); 
     }
-     
      
 }
