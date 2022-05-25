d814924505e1c70c4d943fa37d3b34767ce3d40f
XXNE1 <55995962+XXNE1@users.noreply.github.com>
2022-05-25 22:35:01 +0200

    update BloonGame.java

--- BloonGame/src/bloongame/BloonGame.java
+++ BloonGame/src/bloongame/BloonGame.java
@@ -2,7 +2,9 @@ package bloongame;
 
 public class BloonGame {
 
-    public static void main(String[] args) {
+    public static void public static void main (String[] args) {
+        
+    
        Fenster Fenster = new Fenster();
 
     }
